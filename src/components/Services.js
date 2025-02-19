884a425c65f2b67d4359aabf2ceecbb15e62f0ee
Jaime-Linares <jailinbar@alum.us.es>
2025-02-19 17:15:51 +0100

    [feat] update services information

--- src/components/Services.js
+++ src/components/Services.js
@@ -1,32 +1,27 @@
 import React from "react";
 import "../styles/Services.css";
-import { FaEnvelope, FaCalendarCheck, FaLock, FaBusinessTime, FaRegAddressBook } from "react-icons/fa"; 
+import { FaCalendarCheck, FaBusinessTime, FaRegAddressBook } from "react-icons/fa"; 
+
+
 
 const servicesData = [
   {
-    title: "Mensajes Personalizados",
-    description: "Permite enviar mensajes personales tras el fallecimiento con validación.",
-    price: "Gratis",
-    icon: <FaEnvelope />, 
-    type: "user", 
-  },
-  {
     title: "Esquelas Digitales",
     description: "Crea y envía cuando quieras esquelas personalizadas para tus seres queridos.",
-    price: "$1.99",
+    price: "1.99€/esquela",
     icon: <FaCalendarCheck />, 
     type: "user", 
   },
   {
-    title: "Contratos de Servicios",
-    description: "Crea contratos de servicios funerarios para garantizar la planificación.",
-    price: "$0.99/mes",
+    title: "Plan anual",
+    description: "Deja mensajes totalmente personalizados que serán enviados tras la confirmación de tu fallecimiento.",
+    price: "0.99€/mes",
     icon: <FaRegAddressBook />,
     type: "user", 
   },
   {
     title: "Publicidad para Empresas",
-    description: "Publicita tu funeraria o servicios funerarios en la plataforma.",
+    description: "Publicita tu empresa relacionada con el sector funerario (funeraria, notaria, floristería, etc.) para que más clientes puedan conocer tus servicios.",
     price: "9.99€/mes",
     icon: <FaBusinessTime />, 
     type: "company", 
@@ -38,7 +33,7 @@ const Services = () => {
     <section id="services" className="services">
       <div className="container">
         <h2>Nuestros Servicios</h2>
-        <p>Ofrecemos una plataforma segura para gestionar tu legado digital y servicios funerarios.</p>
+        <h3>Ofrecemos una plataforma segura para gestionar tu legado digital y comunicación post-mortem.</h3>
 
         <div className="service-grid">
           {servicesData.map((service, index) => (
@@ -46,7 +41,7 @@ const Services = () => {
               <div className="icon">{service.icon}</div>
               {service.type === "company" && <span className="badge">Empresas</span>}
               <h3>{service.title}</h3>
-              <p>{service.description}</p>
+              <p style={{ textAlign: "center" }}>{service.description}</p>
               <p className="price">{service.price}</p>
             </div>
           ))}
