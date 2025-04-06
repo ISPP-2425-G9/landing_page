e889cc7d5c3d66feb23877247ea3f1dc9337bc9b
jormunrod <jorgemr@protonmail.ch>
2025-04-06 17:36:05 +0200

    feat: implement Ads component with video display and styling

--- src/components/Ads.js
+++ src/components/Ads.js
@@ -1,18 +1,32 @@
-import React, { useEffect } from "react";
-import { FaFacebook, FaGithub } from "react-icons/fa";
-import {
-  FaInstagram,
-  FaLinkedin,
-  FaTiktok,
-  FaWhatsapp,
-  FaXTwitter,
-} from "react-icons/fa6";
-import "../styles/Contact.css";
+import React from "react";
+import "../styles/Ads.css";
 
 const Ads = () => {
-  
+  const videos = [
+    { id: "ZT6DLKNmrzY", title: "First Video Title" },
+    { id: "ZT6DLKNmrzY", title: "Second Video Title" },
+  ];
+
   return (
-    <h1>Ads</h1>
+    <div>
+      <h1>Ads</h1>
+      <div className="video-container">
+        {videos.map((video) => (
+          <div key={video.id} className="video-item">
+            <h2 className="video-title">{video.title}</h2>
+            <iframe
+              width="560"
+              height="315"
+              src={`https://www.youtube.com/embed/${video.id}`}
+              title="YouTube video player"
+              frameBorder="0"
+              allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+              allowFullScreen
+            ></iframe>
+          </div>
+        ))}
+      </div>
+    </div>
   );
 };
 
